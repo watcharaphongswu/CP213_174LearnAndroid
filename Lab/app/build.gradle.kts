61a8d098c20105a5a14a131d73ce677b56b82a53
Watcharaphong09 <145739469+Watcharaphong09@users.noreply.github.com>
2026-01-27 15:50:14 +0700

    f

--- Lab/app/build.gradle.kts
+++ Lab/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.a174lablearnandroid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.a174lablearnandroid"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
---
+++ Lab/app/src/main/java/com/example/a174lablearnandroid/MainActivity2.kt
@@ -0,0 +1,71 @@
+package com.example.a174lablearnandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a174lablearnandroid.ui.theme._174LabLearnAndroidTheme
+
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+//            _517LabLearnAndroidTheme {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Greeting2(
+                    name = "Android2",
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+//            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    var inputText by remember { mutableStateOf("") }
+
+    Column {
+        Text(
+            text = "Hello $name! say = " + inputText,
+            modifier = modifier
+        )
+        TextField(
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            }
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    _174LabLearnAndroidTheme {
+        Greeting2("Android")
+    }
+}
